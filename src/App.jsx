201ef925e2853079750a582062392faa2895bf9f
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-01-23 23:12:25 -0800

    create initial files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import img from "./assets/1.png"
+function App() {
+  return (
+    <>
+      <h1>Book App Starting files</h1>
+      <img src={img} alt="uuu" />
+    </>
+  );
+}
+
+export default App;
